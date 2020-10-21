cd69d42821d1e0b9335c3de2623f019f4e2b0de5
khanhnhb <hello311016@gmail.com>
2020-10-21 13:21:40 +0700

    Import File, Upload File excel, Call API Get invoice

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,3 +1,7 @@
 export const BASE_URL = "http://localhost:8090";
-
 export const LOGIN_ENDPOINT = `${BASE_URL}/auth/signin/admins`;
+export const INVOICE_ENDPOINT = `${BASE_URL}/invoices`;
+
+
+export const BASE_URL_BLOCKCHAIN = "http://localhost:8081";
+export const BLOCKCHAIN_INVOICES_ENDPOINT = `${BASE_URL_BLOCKCHAIN}/api/blockchain/invoices`;
