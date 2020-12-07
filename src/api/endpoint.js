af5fa997617c2e0c54f0385b89515d3365940dfb
khanhnhb <hello311016@gmail.com>
2020-12-08 01:10:51 +0700

    Endpoint BC

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://18.141.214.35:8090";
-// export const BASE_URL = "http://34.126.70.182:8090";
+// export const BASE_URL = "http://18.141.214.35:8090";
+export const BASE_URL = "http://34.126.70.182:8090";
 // export const BASE_URL = "http://localhost:8090";
 export const LOGIN_ENDPOINT = `${BASE_URL}/auth/signin/admins`;
 
@@ -13,6 +13,6 @@ export const PROVIDER_ENDPOINT = `${BASE_URL}/providers`;
 export const PROFILE_ENDPOINT = `${BASE_URL}/auth/profile`;
 export const DELIVERIES_STATUS_ENDPOINT = `${BASE_URL}/deliveries-status`;
 
-export const BASE_URL_FABRIC = `http://192.168.1.18:4000`;
+export const BASE_URL_FABRIC = `http://34.126.123.79:4000`;
 // export const BASE_URL_BLOCKCHAIN = "http://18.141.214.35:8090";
 // export const BLOCKCHAIN_INVOICES_ENDPOINT = `${BASE_URL_BLOCKCHAIN}/blockchain/invoices/codes/`;
